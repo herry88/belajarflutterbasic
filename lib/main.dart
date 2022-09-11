1479f4e7f21c9d6b6d3a6c51c2e20fe758b29bd4
Herry Prasetyo <herry081288@gmail.com>
2022-09-11 15:22:35 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //titleSection
+  Widget titleSection = Container(
+    child: Row(
+      children: [
+        Expanded(
+          child: Text('Hello World'),
+        ),
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Layout',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('BelajarFlutter'),
+          title: Text('Flutter Layout'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'image/lake.jpg',
+              width: 600,
+              height: 400,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+          ],
         ),
       ),
     );
   }
 }
-
